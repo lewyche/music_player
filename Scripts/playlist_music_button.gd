e53443d819380495332e87ef594d8d9adce57cd3
Robert <robertyan64913@gmail.com>
2022-07-20 21:26:15 -0400

    removed irrelevant files

---
+++ Scripts/playlist_music_button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+onready var music_controls = get_node("../../../../../Music Controls")
+onready var music_player = get_node("../../../../../../music_player")
+onready var playlist_songs = get_node("../../../../Playlist_songs")
+
+
+func set_text(txt):
+	text = txt
+
+func set_current_song():
+	music_controls.change_current_song(text)
+	
+func start_playing():
+	music_player.set_music_stream("res://Music/" + text)
+
+func _ready():
+	clip_text = false
+
+func _on_playlist_music_button_pressed():
+	set_current_song()
+	start_playing()
+	playlist_songs.ready_delete(text)
